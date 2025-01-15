43c2f5784be695d271cc0fb5016dda11ceaf8aae
go52xep <ajla.muslija@tum.de>
2025-01-15 09:38:38 +0100

    research articles can be written to csv.

--- testing_crossref.py
+++ testing_crossref.py
@@ -1,13 +1,17 @@
 from crossref.restful import Works, Etiquette
+import json
+
 my_ettiquete = Etiquette('Sunrise Prototype', '1.0', 'N/A', 'r.harizaj@uni-muenster.de')
 
 works = Works(etiquette=my_ettiquete)
 
 
-ai_works = works.query("AI Academia Industry")
-ai_works = ai_works.filter(has_abstract="true")
-ai_works = ai_works.sort("issued")
+ai_works = works.query("AI").query("Academia").query("Industry").filter(has_abstract="true").sort("issued")
 
-sample_works = ai_works.sample(5)
 
-print(sample_works.count)
+with open("research_articles", "w") as research:
+    ai = ai_works.sample(10)
+    
+    for i in ai:
+        research.write(f"\"{i.get("title")}\", \"{i.get("abstract")}\", {i.get("issued")}")
+        research.write("\n")
