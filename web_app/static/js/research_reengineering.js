cc4bf3f822c34527058ef6257c4f967ab80523f8
ri-lind <lindiharizaj@gmail.com>
2025-01-21 11:26:20 +0100

    refactoring successful

--- web_app/static/js/research_reengineering.js
+++ web_app/static/js/research_reengineering.js
@@ -1,3 +1,3 @@
-import { handleSentimentGeneration } from 'utilities/sentiment_generation.js';
+import { handleSentimentGeneration } from './utilities/sentiment_generation.js';
 // Add the event listener and call the new function
 document.getElementById('submit-claim').addEventListener('click', handleSentimentGeneration);
